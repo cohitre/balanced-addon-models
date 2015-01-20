b15d39f586f276cd6352af8780c3ae22e18224e6
cohitre <carlosrr@gmail.com>
2015-01-19 16:58:12 -0800

    Updating api key error handler to handle 1.0 response

--- addon/models/error-handlers/api-key-production.js
+++ addon/models/error-handlers/api-key-production.js
@@ -1,6 +1,17 @@
 //import Ember from "ember";
 import BaseErrorHandler from "./base";
 
-var ApiKeyProductionErrorHandler = BaseErrorHandler.extend({});
+var ApiKeyProductionErrorHandler = BaseErrorHandler.extend({
+  populateFromResponse: function(response) {
+    response = response.responseJSON || response;
+
+    if (!Ember.isBlank(response.description)) {
+      this.addRootError(response.description);
+    }
+    else {
+      return this._super(response);
+    }
+  }
+});
 
 export default ApiKeyProductionErrorHandler;
